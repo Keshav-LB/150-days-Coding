b0c68c3de85207b872720acd4d2e64c786fdd1cc
Keshav <keshavmishra1001@gmail.com>
2025-08-04 19:23:48 +0530

    concat string

---
+++ src/com/leetcode/basic/Strings/ConcatString.java
@@ -0,0 +1,10 @@
+package com.leetcode.basic.Strings;
+
+public class ConcatString {
+	public static void main(String[] args) {
+		String str1 = "Hello";
+		String str2 = "world";
+		
+		System.out.println(str1.concat(str2));
+	}
+}
