1b14d712126c646f919b2970e16129e74e8dc03e
Keshav <keshavmishra1001@gmail.com>
2025-08-04 14:27:51 +0530

    pattern 9

---
+++ src/com/leetcode/patterns/Pattern9.java
@@ -0,0 +1,20 @@
+package com.leetcode.patterns;
+
+public class Pattern9 {
+	public static void main(String[] args) {
+//	    1
+//	   2 3
+//	  4 5 6
+//	 7 8 9 10
+		int num=1;
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=(4-i);j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print(num+++" ");
+			}
+			System.out.println();
+		}
+	}
+}
