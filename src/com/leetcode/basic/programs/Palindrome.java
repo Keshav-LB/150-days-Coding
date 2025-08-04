d88ab4a0ce5141119f4cc405a8b7e31c12c6d54f
Keshav <keshavmishra1001@gmail.com>
2025-08-04 11:28:55 +0530

    palindrome

---
+++ src/com/leetcode/basic/programs/Palindrome.java
@@ -0,0 +1,18 @@
+package com.leetcode.basic.programs;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		int num = 15351;
+		int temp=num;
+		int rem, rev=0;
+		while(num>0) {
+			rem = num%10;
+			rev=rev*10+rem;
+			num/=10;
+		}
+		if(temp==rev)
+			System.out.print("Palindrome");
+		else
+			System.out.println("Not a Palindrome");
+	}
+}
