b8cf2559ec118a72235fc9b12c1c89bfed43a6da
Keshav <keshavmishra1001@gmail.com>
2025-08-04 11:22:08 +0530

    first n natural nums

---
+++ src/com/leetcode/basic/programs/FirstNnaturalNumber.java
@@ -0,0 +1,12 @@
+package com.leetcode.basic.programs;
+
+public class FirstNnaturalNumber {
+	public static void main(String[] args) {
+		int num=10;
+		int i=1;
+		while(i<=num) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
