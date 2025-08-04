65b557a73d69ec92a50c356a3bcbf133bf8fc341
Keshav <keshavmishra1001@gmail.com>
2025-08-04 18:14:55 +0530

    spiral matrix

---
+++ src/com/leetcode/basic/arrays/SpiralMatrix.java
@@ -0,0 +1,41 @@
+package com.leetcode.basic.arrays;
+
+public class SpiralMatrix {
+	public static void main(String[] args) {
+		int[][] arr = {
+        		{1, 2, 3},
+        		{4, 5, 6},
+        		{7, 8, 9}
+    		  };
+		
+//		o/p: 1 2 3 6 9 8 7 4 5
+		
+		int top = 0;
+		int bottom = arr.length-1;
+		int left = 0;
+		int right = arr[0].length-1;
+		
+		while(top <= bottom && left <= right) {
+			
+			for(int i=left;i<=right;i++) {
+				System.out.print(arr[top][i]+" ");
+			}
+			top++;
+			
+			for(int i=top;i<=bottom;i++) {
+				System.out.print(arr[i][right]+" ");
+			}
+			right--;
+			
+			for(int i=right;i>=left;i--) {
+				System.out.print(arr[bottom][i]+" ");
+			}
+			bottom--;
+			
+			for(int i=top;i<=bottom;i++) {
+				System.out.print(arr[i][left]+" ");
+			}
+			left++;
+		}
+	}
+}
