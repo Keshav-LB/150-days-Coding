b9cb708d3a77e566903529d0eeb9b3ea56a4af5a
Keshav <keshavmishra1001@gmail.com>
2025-08-04 11:08:04 +0530

    firstcommit

---
+++ src/com/leetcode/basic/programs/Armstrong.java
@@ -0,0 +1,19 @@
+package com.leetcode.basic.programs;
+
+public class Armstrong {
+	public static void main(String[] args) {
+		int num = 153,sum=0;
+		int temp=num;
+		while(num>0) {
+			int rem = num%10;
+			sum+=Math.pow(rem, 3);
+			num/=10;
+		}
+		if(temp == sum) {
+			System.out.print("Armstrong num.");
+		}
+		else {
+			System.out.println("Not a Armstrong");
+		}
+	}
+}
