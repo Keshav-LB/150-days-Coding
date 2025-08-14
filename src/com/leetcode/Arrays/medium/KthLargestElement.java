a4dcaf497ee725e00e7569885505659986988e82
Keshav <keshavmishra1001@gmail.com>
2025-08-15 00:44:38 +0530

    leetcode 215. kth largest element

--- src/com/leetcode/Arrays/medium/KthLargestElement.java
+++ src/com/leetcode/Arrays/medium/KthLargestElement.java
@@ -5,16 +5,19 @@ import java.util.PriorityQueue;
 public class KthLargestElement {
 	public static void main(String[] args) {
 		int[] nums = {3, 2, 3, 1, 2, 4, 5, 5, 6};
-		int k = 5;
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int k = 3;
+		
+		System.out.println(k+" largest : "+kthLargest(nums, k));
+	}
+	public static int kthLargest(int nums[], int k) {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 		
 		for(int num : nums) {
 			pq.add(num);
-			if(pq.size()>k) {
+			if(pq.size() > k) {
 				pq.poll();
 			}
 		}
-		
-		System.out.println(k+" largest element : "+pq.peek());
+		return pq.peek();
 	}
 }
