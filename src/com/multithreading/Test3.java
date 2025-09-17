cb1f45d2f272b347f35ec82656032687570289c2
Keshav <keshavmishra1001@gmail.com>
2025-09-17 22:15:25 +0530

    creating thread by implementing runnable interface

---
+++ src/com/multithreading/Test3.java
@@ -0,0 +1,26 @@
+package com.multithreading;
+
+	//by implementing runnable interface
+class MyRunnable implements Runnable{
+
+		@Override
+		public void run() {
+			for(int i=0; i<5; i++) {
+				System.out.println("           child-thread      ");
+			}
+		}		
+}
+
+public class Test3 {
+	public static void main(String[] args) {
+		
+		MyRunnable r = new MyRunnable();
+		Thread t = new Thread(r);
+		
+		t.start();
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("parent-thread");
+		}
+	}
+}
